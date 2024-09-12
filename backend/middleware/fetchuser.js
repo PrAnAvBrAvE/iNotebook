5e63fd490deaaa64ab7c7673ecc98fb0f13ea684
Pranav Kumar Patil <patilpranav8055@gmail.com>
2024-09-12 18:25:12 +0530

    Update fetchuser.js

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,5 +1,5 @@
 var jwt = require('jsonwebtoken');
-const JWT_SECRET = 'Pranavisagoodb$oy'
+const JWT_SECRET = 'YOUR_API_KEY'
 
 const fetchuser = (req, res, next) => {
     // Get the user from the jwt token and add id to req object
